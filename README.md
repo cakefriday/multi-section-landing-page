0bdae76895f98a5c8d8458bc8a37f6e8af66cb02
Anita Lehmann <anita@l3h.de>
2023-03-27 20:30:27 +0200

    Commit for the whole project

--- README.md
+++ README.md
@@ -1,13 +1,66 @@
-# Landing Page Project
+ # Landing Page Project by Udacity
+### Building a navigation dynamically and highlighting sections in viewport when scrollen or scrolling to section when clicked
+
+---
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+-   [Project Title](#Udacity-|-Landing-Page-Project)
 
-## Instructions
+-   [Table of contents](#table-of-contents)
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+-   [Installation & Usage](#installation-and-usage)
 
-To get started, open `js/app.js` and start building out the app's functionality
+-   [Development](#development)
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+-   [Contribute](#contribute)
+
+-   [License & Copyright](#License-&-Copyright)
+
+---
+
+## Installation and Usage
+
+[(Back to top)](#table-of-contents)
+
+You can install this project by ZIP file or cloning on github
+
+---
+
+## Development
+
+[(Back to top)](#table-of-contents)
+
+**_JS Version: ES2015/ES6._**
+
+**_JS Standard: ESlint_**
+
+### **The Project Contains Four Files :**
+
+#### *css/styles.css*
+
+#### *index.html*
+
+#### *js/app.js*
+
+#### *README.md*
+
+The buildNav functions dynamically creates a navigation bar out of sections found in the HTML code. the navigation bar is made of links in a list. When a navbar link is clicked, the page scrolls smoothly to the section.
+
+The setActive function adds a class to the section and navbar links to highlight the section in the navigation bar that is in view on the page while scrolling the page. 
+---
+
+# Contribute
+
+[(Back to top)](#table-of-contents)
+
+**- Udacity**
+
+**- https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/**
+---
+
+# License & Copyright
+
+[(Back to top)](#table-of-contents)
+
+_© Udacity - Modyfied by Anita Lehmann_
--- js/app.js
+++ js/app.js
@@ -1,62 +1,104 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
 /**
  * Define Global Variables
- * 
-*/
-
-
+ *
+ */
+// Variables will be used in different functions
+const sections = document.getElementsByTagName("section");
+const navList = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
+  // Function returns true if the element top is near the top of the viewport, false otherwise
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.top <=
+      0.4 * (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
 
+function getElementOffset(element) {
+  const rect = element.getBoundingClientRect();
+  return {
+    top: rect.top + window.pageYOffset,
+    left: rect.left + window.pageXOffset
+  };
+}
 
 /**
- * End Helper Functions
+ * End Helper Function
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
-// build the nav
+// building the Navigation dynamically section by section
+function buildNav() {
+    const fragment = document.createDocumentFragment();
+    for (let section of sections) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = section.dataset.nav;
+        a.setAttribute("class", "menu__link");
+        a.setAttribute("id", section.id);
+        a.addEventListener("click", () => {
+            section.scrollIntoView({behavior: "smooth"});
+            });
+        li.appendChild(a);
+        fragment.appendChild(li);
+    
+    }
+navList.appendChild(fragment);
+}
 
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+function setActive() {
+  for (const section of sections) {
+    const idString = String(section.id);
+    const activeLink = document.querySelector(`#${idString}`);
+    if (isInViewport(section)) {
+      section.classList.add("your-active-class");
+      activeLink.classList.add("menu__link--active");
+    } 
+    else {
+      section.classList.remove("your-active-class");
+      activeLink.classList.remove("menu__link--active");
+    }
+  }
+}
 
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+document.addEventListener("DOMContentLoaded", function() {
+  buildNav();
+});
 
-// Scroll to section on link click
-
-// Set sections as active
-
+window.addEventListener("scroll", function() {
+  setActive();
+});
 
